77e9ad6cd879163ab7a46d4c0655dd632cef5f74
Shigeyuki Takeuchi <take@cyclogy.com>
2016-01-30 16:46:16 +0900

    Add Carhage support - Add Cocoa Touch Framework target - Share framework scheme

---
+++ RKDropdownAlertFramework/RKDropdownAlertFramework.h
@@ -0,0 +1,14 @@
+//
+//  RKDropdownAlertFramework.h
+//  RKDropdownAlertFramework
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RKDropdownAlertFramework.
+FOUNDATION_EXPORT double RKDropdownAlert_FrameworkVersionNumber;
+
+//! Project version string for RKDropdownAlertFramework.
+FOUNDATION_EXPORT const unsigned char RKDropdownAlert_FrameworkVersionString[];
+
+#import "RKDropdownAlert.h"
